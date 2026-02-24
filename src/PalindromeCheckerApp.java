44b1f29adbe6e3b8fc2db1710ea24a8be7abd19a
ADKCODES <aadhif1@gmail.com>
2026-02-24 14:56:48 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,8 @@
 public class PalindromeCheckerApp {
     public class void main(String[] args){
+        System.out.println("welcome to the palindrome checker management system");
+        System.out.println("version : 1.0");
+        System.out.println("System initailised successfull");
 
     }
 }
