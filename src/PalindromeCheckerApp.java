01e32adbd41074d0dd05853020ee3dcdf43b5252
ADKCODES <aadhif1@gmail.com>
2026-02-24 16:38:57 +0530

    UC4 DONE

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,27 +1,35 @@
 public class PalindromeCheckerApp {
     public class void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== Palindrome Checker App (UC3) ===");
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
+        String input = "radar";
 
-        String reversed = "";
 
-        // Iterate from the last character to the first
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        char[] chars = input.toCharArray();
+
+
+        int start = 0;
+
+
+        int end = chars.length - 1;
+
+
+        boolean isPalindrome = true;
+
+
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+
+            start++;
+            end--;
         }
 
-        // Compare original and reversed string
-        if (input.equals(reversed)) {
-            System.out.println("Result: The string is a PALINDROME.");
-        } else {
-            System.out.println("Result: The string is NOT a palindrome.");
-        }
 
-        scanner.close();
-
-
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
