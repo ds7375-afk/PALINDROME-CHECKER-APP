97e51baa874d9821da607da11c7d3c6534910f21
ADKCODES <aadhif1@gmail.com>
2026-02-24 16:09:29 +0530

    uc2 done

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,5 +1,17 @@
 public class PalindromeChecker {
     public static void main(String[] args){
-        System.out.println(");
+        String input="Madam";
+        boolean isPalindrome=true;
+        for(int i=0;i<input.length()/2;i++){
+            if(input.charAt(i)!=input.charAt(input.length()-1-i)){
+                isPalindrome=false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        }else{
+            System.out.println("The string \"" + input + "\" is not a palindrome.");
+        }
     }
 }
